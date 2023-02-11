ff360ffe973c0ba7b16aced0ad4e286380c160db
Boone Cabal <cyp-gabriel@outlook.com>
2023-02-11 08:57:29 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Shaffer',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 1980'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 1980'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
